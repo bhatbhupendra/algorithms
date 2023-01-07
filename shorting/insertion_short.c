82e83dc81e99667f4334d993326c650010d9e75b
Bhupendra Bhat <bhupendra420110bhat@gamil.com>
2023-01-07 13:37:11 +0545

    first commit - added insertion_short

---
+++ shorting/insertion_short.c
@@ -0,0 +1,40 @@
+//insertion short
+
+#include<stdio.h>
+
+void main(){
+	int data[] = {9, 5, 1, 4, 3};
+  	int size = sizeof(data) / sizeof(data[0]);
+  	
+  	//insertion short algorithm
+  	int step;
+  	for (step = 1; step < size; step++) {
+	    int key = data[step];
+	    int j = step - 1;
+	
+	    while (key < data[j] && j >= 0) {
+	      data[j + 1] = data[j];
+	      --j;
+	    }
+	    data[j + 1] = key;
+  	}
+  	
+  	//second
+  	
+	  	for (step = 1; step < size; step++) {
+		    int key = data[step];
+		    int j = step;
+		
+		    while (key < data[j-1] && j >= 0) {
+		      data[j] = data[j-1];
+		      --j;
+		    }
+		    data[j] = key;
+	  	}
+  	
+  	//printing the data array
+  	int i;
+  	for (i = 0; i < size; i++) {
+    	printf("%d ", data[i]);
+  	}
+}
