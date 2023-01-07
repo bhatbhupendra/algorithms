50807961f95c4185f0ff0a36eb1b25cd1a6ef960
Bhupendra Bhat <bhupendra420110bhat@gamil.com>
2023-01-07 13:58:42 +0545

    added insertion_short

--- shorting/insertion_short.c
+++ shorting/insertion_short.c
@@ -19,20 +19,29 @@ void main(){
 	    data[j + 1] = key;
   	}
   	
-  	//Alternative way of insertion short
+  	//Alternative way of insertion short which does same as above
+	int data2[] = {9, 5, 1, 4, 3};
+  	int size2 = sizeof(data) / sizeof(data[0]); //calculate the size
   	
-	  	for (step = 1; step < size; step++) {
-		    int key = data[step];
-		    int j = step;
-		
-		    while (key < data[j-1] && j >= 0) {
-		      data[j] = data[j-1];
-		      --j;
-		    }
-		    data[j] = key;
-	  	}
+	for (step = 1; step < size2; step++) {
+	    int key = data2[step];
+	    int j = step;
+	
+	    while (key < data2[j-1] && j >= 0) {
+	      data2[j] = data2[j-1];
+	      --j;
+	    }
+	    data2[j] = key;
+	}
   	
   	//printing the data array
+  	int i;
+  	for (i = 0; i < size; i++) {
+    	printf("%d ", data[i]);
+  	}
+	printf("\n");
+
+	//printing the data2 array
   	int i;
   	for (i = 0; i < size; i++) {
     	printf("%d ", data[i]);
