47088f5f5b03d57c1ccef236e30f5ea430639e85
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2023-01-12 21:59:48 +0545

    Quick & Bubble short added

---
+++ shorting/4_bubble_short.c
@@ -0,0 +1,27 @@
+//also called exchange short
+/* Bubble sort code */
+#include <stdio.h>
+
+int main(){
+  	int i, j,temp;
+
+  	int data[] = {9, 5, 1, 4, 3};
+	int n = sizeof(data) / sizeof(data[0]); //calculate the size
+	
+	for (i = 0 ; i < n - 1; i++){
+	   for (j = 0 ; j < n - 1 - i; j++){ // with i=0 it is true upto j<4
+	    	if (data[j] > data[j+1]){ // For decreasing order use '<' instead of '>'
+		        temp = data[j];
+		        data[j] = data[j+1];
+		        data[j+1] = temp;
+	      	}
+	    }
+	}
+	
+	printf("Sorted list in ascending order:\n");
+	
+	for (i = 0; i < n; i++)
+	    printf("%d\n", data[i]);
+	
+	return 0;
+}
